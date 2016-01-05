f3e7b11f3d5a0fb3a8621976822eac7af37357ed
tontonton <phantom.ness@windowslive.com>
2016-01-05 09:57:49 +0700

    initial project

---
+++ Weather1/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Weather1
+//
+//  Created by ton on 1/4/2559 BE.
+//  Copyright © 2559 bananacoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let parser = XMLParser(url: "https://weather.yahooapis.com/forecastrss?w=12756339&u=c")
+        let weather: Weather = parser.weather
+        var string = String()
+        
+        string = "\(weather.title)\n\n"
+        string += "Current Conditions: \(weather.descriptionOfConditions), \(weather.temp)C\n\n"
+        string += "forecast:\n"
+        for forecast in weather.forecasts {
+            let day = forecast["day"] as! String
+            let descriptionOfConditions = forecast["descriptionOfConditions"] as! String
+            let highTemp = forecast["highTemp"] as! String
+            let lowTemp = forecast["lowTemp"] as! String
+            
+            string += "\(day) - \(descriptionOfConditions). High: \(highTemp) Low: \(lowTemp)\n"
+        }
+        
+        textField.text = string
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Weather1/Weather.swift
@@ -0,0 +1,17 @@
+//
+//  Weather.swift
+//  Weather1
+//
+//  Created by ton on 1/4/2559 BE.
+//  Copyright © 2559 bananacoding. All rights reserved.
+//
+
+import UIKit
+
+class Weather {
+    
+    var title = String()
+    var descriptionOfConditions = String()
+    var temp = String()
+    var forecasts = [AnyObject]()
+}
